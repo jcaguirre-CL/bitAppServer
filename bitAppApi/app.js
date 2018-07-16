504266ceb480b4bd8a19631c3e7f5645cd14384c
jcaguirre-CL <jcaguirrecl@gmail.com>
2018-07-16 09:58:56 -0400

    cambio pwd

--- bitAppApi/app.js
+++ bitAppApi/app.js
@@ -79,7 +79,7 @@ app.post('/api/auth', function(req, res) {
   const body = req.body;
 
   const user = USERS.find(user => user.username == body.username);
-  if(!user || body.password != 'ingeadmin') return res.sendStatus(401);
+  if(!user || body.password != 'incidencias') return res.sendStatus(401);
   
   var token = jwt.sign({userID: user.id}, 'todo-app-super-shared-secret', {expiresIn: '2h'});
   res.send({token});
